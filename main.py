95b76a46da3ddeb8b8bbf36266218dd20f352283
Paulina Formas <polaformas@gmail.com>
2023-05-15 19:13:17 +0200

    pobieranie danych dla wszystkich stron
    dodanie zapytania dla każdej strony i przeszukanie ich html, uzyskane wyniki nalezy odpowiednio zformatowac aby byly jednolite

--- main.py
+++ main.py
@@ -1,13 +1,36 @@
-from requests_html import HTMLSession
+async def getData(query):
+    query ="Loreal+True+Match+The+Foundation+Podkład+1N+Neutral+Undertone"
+    cocolita_url = f'https://www.cocolita.pl/s?q={query}'
+    minti_url = f'https://mintishop.pl/search.php?text={query}'
+    makeup_url = f'https://makeup.pl/search/?q={query}'
 
-s = HTMLSession()
-query = 'Loreal+True+Match+The+Foundation+Podkład+1N'
-cocolita_url = f'https://www.cocolita.pl/s?q={query}'
-minti_url = f'https://mintishop.pl/search.php?text={query}'
-makeup_url = f'https://makeup.pl/search/?q={query}'
-coco_resp   =   s.get(cocolita_url)
-coco_resp.html.render()
+    coco_resp   =   await s.get(cocolita_url)
+    minti_resp  =  await s.get(minti_url)
+    makeup_resp =  await s.get(makeup_url)
 
-minit_resp  = s.get(minti_url)
-makeup_resp = s.get(makeup_url)
-print(coco_resp.html.find('div[data-type="product-list"] div.product-price-container span.discount-price',first=True).text)
+    await coco_resp.html.arender()
+    await minti_resp.html.arender()
+    await makeup_resp.html.arender()
+    coco_price_raw = coco_resp.html.find('div[data-type="product-list"] div.product-price-container span.discount-price',first=True).text
+    minti_price_raw = minti_resp.html.find('section#search strong.price',first=True).text
+    makeup_price_raw = makeup_resp.html.find('div.catalog-products span.price_item',first=True).text
+    return (coco_price_raw,minti_price_raw,makeup_price_raw)
+
+async def main():
+    #query  = input("Podaj nazwe produkut: ")
+    #print(query)
+    prices =  await getData("Loreal+True+Match+The+Foundation+Podkład+1N+Neutral+Undertone")
+    print(prices)
+
+from requests_html import AsyncHTMLSession
+import asyncio
+
+import asyncio
+if asyncio.get_event_loop().is_running(): # Only patch if needed (i.e. running in Notebook, Spyder, etc)
+    import nest_asyncio
+    nest_asyncio.apply()
+
+
+s = AsyncHTMLSession()
+loop = asyncio.get_event_loop()
+loop.run_until_complete(main())

64eec1a273d8ff9bcdbee19283e0b3db5236f11d
Paulina Formas <polaformas@gmail.com>
2023-05-23 21:47:44 +0200

    dodano sprawdzenie czy znaleziono jakies produkty na danej stronie oraz uzyskiwanie nazwy znalezionego produktu TODO poprawic minti

--- main.py
+++ main.py
@@ -11,23 +11,40 @@ async def getData(query):
     await coco_resp.html.arender()
     await minti_resp.html.arender()
     await makeup_resp.html.arender()
-    coco_price_raw = coco_resp.html.find('div[data-type="product-list"] div.product-price-container span.discount-price',first=True).text
-    minti_price_raw = minti_resp.html.find('section#search strong.price',first=True).text
-    makeup_price_raw = makeup_resp.html.find('div.catalog-products span.price_item',first=True).text
+    #cocolita
+    if (coco_resp.html.find('div.no-items')):
+        coco_price_raw = "Nie znaleziono"
+    else:
+        coco_item = coco_resp.html.find('div[data-type="product-list"] h2.product-name a[data-type="product-url"]',first=True).text
+        coco_price_raw = coco_resp.html.find('div[data-type="product-list"] div.product-price-container span.discount-price',first=True).text
+    #minti
+    if(minti_resp.html.find('div.noproduct_page')):
+        minti_price_raw = "Nie znaleziono"
+    else:
+        minti_item = minti_resp.html.find('section#search h3 a.product_name',first=True).text
+        print(minti_item)
+        minti_price_raw = minti_resp.html.find('section#search strong.price',first=True).text
+    #makeup
+    if (makeup_resp.html.find('div.search-results strong',first=True).text == 0):
+        makeup_price_raw = "Nie znaleziono"
+    else:
+        
+        makeup_item = makeup_resp.html.find('div.catalog-products div.info-product-wrapper a',first=True).text
+        makeup_price_raw = makeup_resp.html.find('div.catalog-products span.price_item',first=True).text
     return (coco_price_raw,minti_price_raw,makeup_price_raw)
 
 async def main():
     #query  = input("Podaj nazwe produkut: ")
     #print(query)
+    
     prices =  await getData("Loreal+True+Match+The+Foundation+Podkład+1N+Neutral+Undertone")
     print(prices)
 
 from requests_html import AsyncHTMLSession
+import nest_asyncio
 import asyncio
 
-import asyncio
-if asyncio.get_event_loop().is_running(): # Only patch if needed (i.e. running in Notebook, Spyder, etc)
-    import nest_asyncio
+if asyncio.get_event_loop().is_running():
     nest_asyncio.apply()
 
 
